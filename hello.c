11c06d2f8d39aea3c0b26c5af87845c7aa84942e
shruti verma <80059433+superv13@users.noreply.github.com>
2026-01-27 10:55:44 +0530

    Update greeting message in hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Shruti here, taking on the world, woohoooooo!\n");
+    printf("Hello, Shruti here, taking on the world, woohoooooo!\n 27012026 - and we are still on jenkins");
     return 0;
 }
