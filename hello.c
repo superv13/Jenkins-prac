1f44135d30264f417228b99894b7d22ad48747c5
shruti verma <80059433+superv13@users.noreply.github.com>
2026-01-27 11:00:33 +0530

    Fix formatting of printf statement in hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Shruti here, taking on the world, woohoooooo!\n 27012026 - and we are still on jenkins\n but it's fine we are learning...");
+    printf("Hello, Shruti here, taking on the world, woohoooooo!\n 27012026 - and we are still on jenkins\n but it's fine we are learning...")
     return 0;
 }
