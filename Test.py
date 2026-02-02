25ea28f35d455ba14108d34a8cebbcefcdea771d
shruti verma <80059433+superv13@users.noreply.github.com>
2026-02-02 16:59:00 +0530

    Refactor Test.py to include unit tests
    Removed pipeline stages and added unit tests for summation function.

--- Test.py
+++ Test.py
@@ -1,31 +1,13 @@
-pipeline {
-    agent any
+import unittest
 
-    stages {
-        stage('Build') {
-            steps {
-                echo 'This is job building stage'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'This is testing stage'
-            }
-        }
-        stage('Staging') {
-            steps {
-                echo 'This is staging environment'
-            }
-        }
-        stage('Deployment') {
-            steps {
-                echo 'This is deployment stage'
-            }
-        }
-        stage('Monitor') {
-            steps {
-                echo 'This is monitoring stage'
-            }
-        }
-    }
-}
+from Prog1 import summation
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        test case to add 2 numbers 
+        """
+        data = [23,56]
+        result = summation(data)
+        self.assertEqual(result, 79)
+if __name__ == '__main__':
+    unittest.main()
